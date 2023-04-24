363ff90f4ab1a17c3930c4224f7fd1ba7f1bec9c
afonsoc <afonsoc@LAPTOP-HUJ523GB>
2023-04-24 17:56:06 +0200

    mise à jour de la fonction similar_names

--- src/main/java/fr/uparis/beryllium/TerminalApplication.java
+++ src/main/java/fr/uparis/beryllium/TerminalApplication.java
@@ -51,7 +51,9 @@ public class TerminalApplication {
                 if (station1.trim().equalsIgnoreCase("lp")) break;
                 chosen_1 = multi_choice(station1,m,scanner);
                 if(chosen_1 == null){
-                    System.out.println("Try again!");
+                    ArrayList<Station> list_1 = similar_names(StringUtils.stripAccents(station1),m);
+                    if(!list_1.isEmpty()) chosen_1 = multi_choice_similar(list_1, scanner);
+                    else System.out.println("Try again!");
                 }
             }
             if (station1.trim().equalsIgnoreCase("quit")) break;
@@ -72,10 +74,11 @@ public class TerminalApplication {
                     System.out.println("Empty String");
                 }
                 if (station2.trim().equalsIgnoreCase("quit")) break;
-                if (station2.trim().equalsIgnoreCase("lp")) break;
-                chosen_2 = multi_choice(station2, m, scanner);
-                if (chosen_2 == null) {
-                    System.out.println("Try again!");
+                chosen_2 = multi_choice(station2,m,scanner);
+                if(chosen_2 == null){
+                    ArrayList<Station> list_2 = similar_names(StringUtils.stripAccents(station2),m);
+                    if(!list_2.isEmpty()) chosen_2 = multi_choice_similar(list_2, scanner);
+                    else System.out.println("Try again!");
                 }
             }
             if (station2.trim().equalsIgnoreCase("quit")) break;
@@ -175,8 +178,6 @@ public class TerminalApplication {
             return stations.get(num_chosen-1);
         }else if(stations.size() == 0){
             System.out.println("No station with the name "+name+ " was found !");
-            ArrayList<Station> s = similar_names(name,m);
-            System.out.println(s);
             return null;
         } else {
             return stations.get(0);
@@ -218,6 +219,38 @@ public class TerminalApplication {
         m.addStation(latitude, longitude, name);
     }
 
+    /**
+     * This method propose every possibilities found for a station name
+     * @param possibilities List of possibilities
+     * @param scanner Same scanner for the whole app.
+     * @return chosen Station
+     */
+    public static Station multi_choice_similar(ArrayList<Station> possibilities, Scanner scanner){
+            System.out.println("But possibilites found. Choose one from the list below:");
+            int i = 1;
+            for(Station s : possibilities){
+                ArrayList<String> neighborLines = s.getNeighboringLines();
+                if (neighborLines.isEmpty()) {
+                    System.out.println(i + ") " + s + ": Terminus (pas de correspondances)");
+                } else {
+                    System.out.println(i + ") " + s + ": " + s.getNeighboringLines());
+                }
+                i++;
+            }
+            int num_chosen = 0;
+            while(num_chosen == 0 || num_chosen>i-1){
+                try {
+                    String read = scanner.nextLine();
+                    if(read.trim().equalsIgnoreCase("quit")) break;
+                    num_chosen = Integer.parseInt(read);
+                    System.out.println(possibilities.get(num_chosen-1) + " was chosen");
+                } catch (Exception e) {
+                    System.out.println("You need to choose a valid number ! Try again ! ");
+                }
+            }
+            return possibilities.get(num_chosen-1);
+    }
+
      /**
      * This method is used to find stations who has similar name in case the user didn't spell correctly
      * @param name Name of the station to look for
@@ -227,17 +260,17 @@ public class TerminalApplication {
      */
     public static ArrayList<Station> similar_names(String name, Map m){
         ArrayList<Station> similar = new ArrayList<>();
-        ArrayList<Station> stations = m.getAllStations();
-        for( Station s : stations) {
-            String nameS = s.getName();
-            if (nameS.length() >= name.length() && nameS.substring(0, name.length()).toLowerCase().contains(name.toLowerCase())) {
+        for( Station s : m.getAllStations()) {
+            String nameS = StringUtils.stripAccents(s.getName());
+            if (nameS.length() >= name.length() && nameS.toLowerCase().contains(name.toLowerCase())) {
                 similar.add(s);
             } else {
                 int sameLetters = 0;
                 int diff = 0;
                 boolean notTheSameWordAnymore = false;
                 int index = 0;
-                while (index != name.length() && index != nameS.length()) {
+                boolean abandon = false;
+                while (index != name.length() && index != nameS.length() && !abandon) {
                     if (!notTheSameWordAnymore) {
                         if (toLowerCase(name.charAt(index)) == toLowerCase(nameS.charAt(index))) sameLetters++;
                         else {
@@ -245,19 +278,17 @@ public class TerminalApplication {
                             diff ++;
                         }
                     } else {
-                        if (index + 1 < nameS.length() && toLowerCase(name.charAt(index)) == toLowerCase(nameS.charAt(index+1))
-                                || toLowerCase(name.charAt(index - 1)) == toLowerCase(nameS.charAt(index))) {
+                        if (diff >=2 ) abandon = true;
+                        else if (index + 1 < nameS.length() && toLowerCase(name.charAt(index)) == toLowerCase(nameS.charAt(index+1))
+                                || toLowerCase(name.charAt(index - 1)) == toLowerCase(nameS.charAt(index)) || toLowerCase(name.charAt(index)) == toLowerCase(nameS.charAt(index)) ) {
                             sameLetters++;
-                        }else{
-                            diff ++;
-                        }
+                            notTheSameWordAnymore = false;
+                        }else diff ++;
                     }
                     index++;
                 }
                 if(sameLetters != 0)
-                System.out.println("même lettre : "+  diff + " et same : " + sameLetters  + " "+ s.getName() + " "+ (diff - sameLetters));
-
-                if (sameLetters != 0   && diff - sameLetters < 3  && diff - sameLetters > 0){
+                if (!abandon && sameLetters >=3){
                     similar.add(s);
                 }
             }
