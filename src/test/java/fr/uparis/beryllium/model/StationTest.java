58f5c54663b51fe60555772f4aa9fb67c8c2f97c
HAMITOUCHE boukhalfa <hamitoucheboukhalfa2019@outlook.fr>
2023-05-02 23:04:18 +0200

    remove unused functions + add alert in js

--- src/test/java/fr/uparis/beryllium/model/StationTest.java
+++ src/test/java/fr/uparis/beryllium/model/StationTest.java
@@ -55,33 +55,5 @@ class StationTest {
         assert (station1.getNextStations().get(voisin2).get(0).getLine().equals(walkingLine));
         assert (station1.getNextStations().get(voisin3).get(0).getLine().equals(walkingLine));
     }
-    
-    // faux/modifier remove walkingneighbors
-    @Test
-    void removeWalkingNeighbours() {
-        // Given
-        Line line = new Line("--MARCHE--");
-        Station station1 = new Station("Félix Faure", new Location(48.84268433479664, 2.2918472203679703));
-        Station station2 = new Station("Commerce", new Location(48.84461151236847, 2.293796842192864));
-        Station station3 = new Station("Opéra", new Location(48.87059812248669, 2.332135072925294));
-        ArrayList<Station> allStations = new ArrayList<>(List.of(station1, station2, station3));
 
-        java.util.Map<Station, ArrayList<NeighborData>> nextS = new HashMap<>();
-        ArrayList<NeighborData> nei = new ArrayList<>();
-        Duration duration = Duration.ZERO;
-        duration.plusSeconds(45);
-        String[] h = {"0","45"};
-        Double dist = 10.0;
-        nei.add(new NeighborData(line, duration, dist));
-        nextS.put(station2, nei);
-        nextS.put(station3, nei);
-        station1.addNextStation(station3, line, h, dist, true);
-
-        // When
-        station1.removeWalkingNeighbours(allStations, 1.0, station1.getLocation(), station3.getLocation());
-
-        // Assert
-        assert (station1.getNextStations().containsKey(station3));
-        assert (!station1.getNextStations().containsKey(station2));
-    }
 }
